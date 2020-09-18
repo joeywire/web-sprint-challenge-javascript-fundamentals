942bad8783297eb92c406888285fb184b24e5039
Joe L <joseph.lasata@gmail.com>
2020-09-18 10:43:40 -0500

    Completed Closure Challenges

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,22 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+/*
+nestedFunction can access the varible `internal` becasue the exist in the same lexical scope. In this case the lexical scope I'm referring to is everything with myFunction's {}. 
 
+The closure is created when we declare nestedFunction in our lexical environment, now when we initalize myFunction and subsequently initiate nestedFunction - nestedFunction has access to all the data in myFunctions lexical environment, including any variables. But this only works in to out, myfunciton can not reach into nested function - its 'enclosed'.
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(param){
+  let count = 0;
+  for(let i = param; i > 0; i--) {
+    count += i;
+  }
+  return count;
+}
+
+console.log(summation(4));
